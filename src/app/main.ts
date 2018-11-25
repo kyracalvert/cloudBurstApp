661f50c343a13e70e7d234a35b88dcf074b54482
Kyra Crawford Calvert <kyracalvert@gmail.com>
2018-11-25 13:50:58 -0600

    add'l cleanup.

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,7 +2,6 @@
 // it bootstraps app.module, which holds the application 
 // and its components
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
